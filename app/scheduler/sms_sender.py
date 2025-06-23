cdc1581481b92b77ec8291b1ee3d5699a461d474
Sivaprasanna <sivaprasannahema25@gmail.com>
2025-06-23 15:36:00 +0530

    commit to delete

--- app/scheduler/sms_sender.py
+++ app/scheduler/sms_sender.py
@@ -2,7 +2,6 @@ import requests
 
 def send_sms(mobile_number,message):
     url = "https://www.fast2sms.com/dev/bulkV2"
-    api_key = "JfHLaXt7gAGru2BNh3FzydDSjEqm0oITeKi8PYkQv16WpswM4Zjv5bJGs6hFxg9ocXuw7BVn8DaSdT4t"
     
     headers = {
         'authorization': api_key,
